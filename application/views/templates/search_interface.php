450c4434067afdf1618bfabb906b3bc1c7aeb704
Danu Pranantha <dpranantha@bol.com>
2018-05-28 20:22:48 +0200

    fix remove 'x' button

--- application/views/templates/search_interface.php
+++ application/views/templates/search_interface.php
@@ -43,8 +43,8 @@
             $this.siblings('.form-control-clear').toggleClass('hidden', !visible);
         }).trigger('propertychange');
         $('.form-control-clear').click(function() {
-            $(this).siblings('input[type="text"]').val('')
-                .trigger('propertychange').focus();
+            $(this).siblings('input[type="text"]').val('');
+            $(this).toggleClass('hidden', true);
             resetSearchText();
         });
     });
